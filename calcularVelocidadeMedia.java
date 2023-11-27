18f64a050ec2b96d82e91469a57e333f19cfa485
Samillyt <141661658+Samillyt@users.noreply.github.com>
2023-11-27 14:30:12 -0300

    Add files via upload

---
+++ calcularVelocidadeMedia.java
@@ -0,0 +1,14 @@
+public class calcularVelocidadeMedia {
+public static void main(String[] args){
+
+    System.out.println("Cálculo da Velocidade Média");
+
+double distanciaPercorrida= 150;
+double tempoGasto= 1.5;
+double velocidadeMedia=distanciaPercorrida/tempoGasto;
+    System.out.println("A distânca percorrida foi de "+distanciaPercorrida+" Km");
+    System.out.println("O tempo gasto foi de "+tempoGasto+" horas");
+    System.out.println("Para encontrar a velocidade média, dividimos a distância percorrida pelo tempo gasto");
+    System.out.println("A velocidade média é "+velocidadeMedia+" Km/h");
+}
+}
